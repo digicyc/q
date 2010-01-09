aa0b9eefc39ea076d15f2aea88be994160cd9f5c
jason <jason@zzq.org>
2010-01-08 18:46:09 -0700

    Added format inline to books

--- src/q/ebooks/admin.py
+++ src/q/ebooks/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from q.ebooks import models
 
+class FormatInline(admin.TabularInline):
+    model = models.Format
+    extra = 5
+
 class BookAdmin(admin.ModelAdmin):
     search_fields = ['title', 'isbn', 'author__firstname', 'author__lastname']
+    inlines = [
+        FormatInline,
+    ]
 
 class FormatAdmin(admin.ModelAdmin):
     search_fields = ['ebook__title', 'ebook__isbn',
