4a4009fec57cd5693df8e1901d2b13fb357970f9
Zach Holmquist <zholmquist@gmail.com>
2010-10-26 23:57:51 -0600

    Cleaned up the views and the urls to look nicer.

--- src/q/ebooks/views.py
+++ src/q/ebooks/views.py
@@ -9,70 +9,55 @@ from q.ebooks.admin import BookAdmin
 from q.ebooks import models
 
 #@login_required
-def index(request):
-    """
-    Search for a book
-    """
+def index(request, template_name="ebooks/index.html"):
+    ctx = {}
     books = None
+    
     if request.GET.has_key('q') and request.GET['q'].strip() != "":
+        template_name = "ebooks/search.html"
         books = admin_keyword_search(models.Book,
-                BookAdmin.search_fields, request.GET['q'])
-        return render_to_response("ebooks/search.html",
-            {
-                'books': books,
-            },
-            context_instance=RequestContext(request)
-        )
+                BookAdmin.search_fields, request.GET['q'])  
+        
     else:
         books = models.Book.objects.order_by("-create_time")[:15]
-
-    return render_to_response("ebooks/index.html",
-        {
-            'books': books,
-        },
-        context_instance=RequestContext(request)
-    )
-
-def books_by_type(request, type, letter="a"):
-    """
-    Browse books by author
-    """
-    if type.lower() == "author":
+    
+    ctx.update({ 'books': books })    
+    return render_to_response(template_name, RequestContext(request, ctx))
+    
+def books_by_type(request, template_name="ebooks/index.html",  *args, **kwargs):
+    ctx = {}
+    
+    filter_type = kwargs.get('type').lower()
+    
+    if filter_type == "author":
+        letter = kwargs.get('letter')
         books = models.Book.objects.filter(authors__lastname__istartswith=letter)
-    elif type.lower() == "title":
-        books  = models.Book.objects.filter(title__istartswith=letter)
+    elif filter_type == "title":
+        books = models.Book.objects.filter(title__istartswith=letter)
+    
+    ctx.update({ 'books': books })  
+    return render_to_response(template_name, RequestContext(request, ctx))
 
-    return render_to_response("ebooks/index.html",
-        {
-            'books': books,
-        },
-        context_instance=RequestContext(request)
-    )
 
-def latest_books_rss(request):
+def latest_books_rss(request, template_name="ebooks/latest_books.rss"):
     """
     Return an RSS feed with the latest 10 books uploaded
     """
+    ctx = {}
     books = models.Book.objects.all().order_by("-create_time")[:10]
 
-    return render_to_response("ebooks/latest_books.rss",
-        {
-            'books': books,
-        },
-        context_instance=RequestContext(request)
-    )
+    ctx.update({ 'books': books })  
+    return render_to_response(template_name, RequestContext(request, ctx))
 
-def book_info(request, book_slug):
+def book_info(request, template_name="ebooks/index.html", *args, **kwargs):
     """
     Display the information for the book
     """
+    ctx = {}
     book = get_object_or_404(models.Book, slug=book_slug)
-
-    return render_to_response("ebooks/book_info.html",
-        {
-            'book': book,
-        },
-        context_instance=RequestContext(request))
+    
+    ctx.update({ 'books': books })  
+    return render_to_response(template_name, RequestContext(request, ctx))
 
 def isbn_search(isbn):
     """
