d533715b1cc3359b89a9fd7d6fe7b21165646e4a
jason <jason@zzq.org>
2010-10-27 23:32:25 -0600

    Added kindle_email to the profile

--- src/q/accounts/models.py
+++ src/q/accounts/models.py
@@ -6,3 +6,10 @@ class UserProfile(models.Model):
     Represents a user's profile
     """
     user = models.ForeignKey(User, unique=True)
+    kindle_email = models.EmailField(blank=True, default="")
+
+    def __str__(self):
+        return "<UserProfile: %s>" % self.user
+
+    def __unicode__(self):
+        return self.__str__()
