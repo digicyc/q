e8094949bc66f50eaaea09f05d82952f28e42d1e
jason <jason@zzq.org>
2012-01-14 13:13:04 -0700

    added a reset activity stream signal

--- q/apps/ebooks/listeners.py
+++ q/apps/ebooks/listeners.py
@@ -1,6 +1,13 @@
 from actstream import actions
 from actstream import action
 
+from django.core.cache import cache
+
 
 def activity_stream_comment(sender, comment, request, *args, **kwargs):
     action.send(request.user, verb='commented on', action_object=comment, target=comment.content_object)
+
+def delete_cache(sender, *args, **kwargs):
+    key = kwargs.get(key, None)
+    if not None:
+        cache.delete(key)
