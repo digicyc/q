50ca82d9fe1cb0c15f71b15cfbf90b13c43db99d
jason <jason@zzq.org>
2012-01-14 13:43:59 -0700

    reset the index actstream on comment.

--- q/apps/ebooks/listeners.py
+++ q/apps/ebooks/listeners.py
@@ -9,3 +9,6 @@ def activity_stream_comment(sender, comment, request, *args, **kwargs):
 
 def clear_all_activity_steam_cache(sender, *args, **kwargs):
     cache.delete("all_activity_stream")
+
+def clear_index_actstream_cache(sender, *args, **kwargs):
+    cache.delete("index_activity_stream")
