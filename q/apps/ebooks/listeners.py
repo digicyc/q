bd1e80dd37b9acf5baa33e8b7d8c2b337b2fcba8
jason <jason@zzq.org>
2012-01-14 13:13:47 -0700

    added a reset activity stream signal

--- q/apps/ebooks/listeners.py
+++ q/apps/ebooks/listeners.py
@@ -8,6 +8,6 @@ def activity_stream_comment(sender, comment, request, *args, **kwargs):
     action.send(request.user, verb='commented on', action_object=comment, target=comment.content_object)
 
 def delete_cache(sender, *args, **kwargs):
-    key = kwargs.get(key, None)
+    key = kwargs.get("key", None)
     if not None:
         cache.delete(key)
