3ade5141270332e2150d3a45ab2ff2e71493bde7
jason <jason@textop.(null)>
2012-01-29 19:28:38 -0700

    broken code.

--- q/apps/api/urls/v2.py
+++ q/apps/api/urls/v2.py
@@ -4,7 +4,7 @@ from api.resources.v2 import books
 
 api = NamespacedApi(api_name='v2', urlconf_namespace='api')
 
-api.register(books.BookResource())
+#api.register(books.BookResource())
 api.register(books.GoodReadsResource())
 
 urlpatterns = patterns('',
