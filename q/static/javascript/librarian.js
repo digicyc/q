7fd0c7a9fa4708f0b92c5d7188b735b074f496e6
jason <jason@zzq.org>
2012-01-30 22:10:28 -0700

    Fixed the ISBN search to not have an isbn hard coded.

--- q/static/javascript/librarian.js
+++ q/static/javascript/librarian.js
@@ -8,7 +8,6 @@ function Book(url, isbn) {
             isbn: this.isbn,
             type: "GET"
         },
-        async: false,
         success: function(data) {
             console.log(data)
             populate(data.objects[0]);
@@ -29,9 +28,10 @@ function populate(book) {
     $("#id_title").val(book.title);
     $("#id_cover_url").val(book.image_url);
     $("#id_authors").val(book.authors.author.name);
+    if (book.series_works.series_work) {
     $("#id_series").val(book.series_works.series_work.series.title);
-    console.log(book.series_works.series_work.user_position);
     $("#id_series_num").val(book.series_works.series_work.user_position);
+    }
     $("#id_title").val(book.title);
     $("#id_isbn10").val(book.isbn);
     $("#id_isbn13").val(book.isbn13);
