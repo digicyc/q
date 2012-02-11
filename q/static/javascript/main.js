dfea6c42ca0e54463eeed7694f2403f04cb73b68
jason <jason@zzq.org>
2012-02-10 20:57:09 -0700

    Ok, so loading fonts and css on demand isn't the best idea.

--- q/static/javascript/main.js
+++ q/static/javascript/main.js
@@ -13,10 +13,6 @@ require(["jquery",
             link.media = "all";
             document.getElementsByTagName("head")[0].appendChild(link);
         }
-        loadCss("https://fonts.googleapis.com/css?family=Oswald");
-        loadCss("https://fonts.googleapis.com/css?family=Play:400,700");
-        loadCss("https://fonts.googleapis.com/css?family=Lobster");
-        loadCss("https://fonts.googleapis.com/css?family=Sorts+Mill+Goudy:400,400italic");
-        loadCss("/font/stylesheet.css");
+
     }
 );
