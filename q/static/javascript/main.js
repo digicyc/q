a7a0c29f8deae01048ed302d14be54f7eee528d2
jason <jason@zzq.org>
2012-02-10 20:46:59 -0700

    Dynamically load the fonts

--- q/static/javascript/main.js
+++ q/static/javascript/main.js
@@ -1,5 +1,6 @@
 require(["jquery",
          "slides.min.jquery",
+
          "backbone-0.9.1",
          "backbone-tastypie-0.1"
         ],
@@ -12,8 +13,10 @@ require(["jquery",
             link.media = "all";
             document.getElementsByTagName("head")[0].appendChild(link);
         }
+        loadCss("https://fonts.googleapis.com/css?family=Oswald");
+        loadCss("https://fonts.googleapis.com/css?family=Play:400,700");
+        loadCss("https://fonts.googleapis.com/css?family=Lobster");
+        loadCss("https://fonts.googleapis.com/css?family=Sorts+Mill+Goudy:400,400italic");
         loadCss("/font/stylesheet.css");
-
-
     }
 );
