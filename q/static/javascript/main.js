a263cd5a246bd5da0ea7937ce36068daa750b6ac
jason <jason@zzq.org>
2012-02-10 20:44:49 -0700

    Dynamicall load the fonts css

--- q/static/javascript/main.js
+++ q/static/javascript/main.js
@@ -1,3 +1,19 @@
-require(["jquery", "slides.min.jquery"], function($) {
+require(["jquery",
+         "slides.min.jquery",
+         "backbone-0.9.1",
+         "backbone-tastypie-0.1"
+        ],
+    function($) {
+        function loadCss(url) {
+            var link = document.createElement("link");
+            link.type = "text/css";
+            link.rel = "stylesheet";
+            link.href = url;
+            link.media = "all";
+            document.getElementsByTagName("head")[0].appendChild(link);
+        }
+        loadCss("/font/stylesheet.css");
 
-});
+
+    }
+);
