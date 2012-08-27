195ced0f9cc9a29514fa98206085f82314ebe7c2
jason <jason@zzq.org>
2012-08-27 04:33:03 -0600

    themed with library bootstrap

---
+++ q/static/js/library.js
@@ -0,0 +1,10 @@
+$(function(){
+	$('.estante li').on('mouseenter', function(){
+		$(this).addClass('pulse');
+	}).on('mouseleave', function(){
+		$(this).removeClass('pulse');
+	});
+	setTimeout(function(){
+		$('.estante li').removeClass('fadeInLeft');
+	},1001);
+});
